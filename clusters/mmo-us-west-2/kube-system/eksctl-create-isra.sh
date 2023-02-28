eb6315644ff4f1f8a92ab802f04276a334946cf7
Biran <birayaha@3c063041fedb.ant.amazon.com>
2023-02-28 13:14:34 -0800

    agones

--- clusters/mmo-us-west-2/kube-system/eksctl-create-isra.sh
+++
@@ -1,7 +0,0 @@
-eksctl create iamserviceaccount \
-  --cluster=mmo-us-west-2 \
-  --namespace=kube-system \
-  --name=aws-load-balancer-controller \
-  --role-name "AmazonEKSLoadBalancerControllerRole" \
-  --attach-policy-arn=arn:aws:iam::124902971876:policy/AWSLoadBalancerControllerIAMPolicy \
-  --approve
--- clusters/mmo-us-west-2/kube-system/helm-install-aws-load-balancer-controller.sh
+++
@@ -1,5 +0,0 @@
-helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
-  -n kube-system \
-  --set clusterName=mmo-us-west-2 \
-  --set serviceAccount.create=false \
-  --set serviceAccount.name=aws-load-balancer-controller 
