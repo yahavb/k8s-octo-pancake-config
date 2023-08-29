d002dc21c9ad35131fb6b531751dab6d76530330
yahavb <yahavb@amazon.com>
2023-08-29 10:14:53 -0700

    cas/karpenter comp

--- clusters/trn-use1/export.sh
+++
@@ -1,7 +0,0 @@
-export KARPENTER_VERSION=v0.29.2
-export AWS_PARTITION="aws" # if you are not using standard partitions, you may need to configure to aws-cn / aws-us-gov
-export CLUSTER_NAME="trn-use1"
-export AWS_DEFAULT_REGION="us-east-1"
-export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
-export TEMPOUT=$(mktemp)
-
